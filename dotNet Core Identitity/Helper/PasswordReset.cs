10e960374a42da741488e8006b9a94125f7033af
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2022-08-24 13:15:24 +0300

    Initial

---
+++ dotNet Core Identitity/Helper/PasswordReset.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace dotNet_Core_Identitity.Helper
+{
+    public class PasswordReset
+    {
+        public static void PasswordResetSendEmail(string link,string email)
+        {
+            MailMessage mail = new MailMessage();
+
+            SmtpClient smtpClient = new SmtpClient("smtp.yandex.com", 587);
+            smtpClient.EnableSsl = true;
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.Credentials = new System.Net.NetworkCredential("questionautomation@yandex.com", "topnjkmpmuqbgcce");
+
+            mail.From = new MailAddress("questionautomation@yandex.com");
+            mail.To.Add(email);
+
+
+            mail.Subject = $"www.bodybody.com:::Şifre Sıfırlama";
+            mail.Body = "<h2>Şifrenizi yenilemek için lütfen aşağıdaki linke tıklayınız.</h2><hr/>";
+            mail.Body += $"<a href='{link}'> şifre yenileme linki</a>";
+            mail.IsBodyHtml = true;
+
+
+            mail.SubjectEncoding = System.Text.Encoding.UTF8;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
+            smtpClient.Send(mail);
+        }
+    }
+}
---
+++ dotNet Core Identitity/Models/AppIdentityDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace dotNet_Core_Identitity.Models
+{
+    public class AppIdentityDbContext:IdentityDbContext<AppUser,AppRole,string>
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options):base(options)
+        {
+
+        }
+    }
+}
